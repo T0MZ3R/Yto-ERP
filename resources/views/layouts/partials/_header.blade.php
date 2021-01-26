c0505fc91ca8529a69e888b28a53853780dc1bf9
YVES <yves.tshimangakabidi@ynov.com>
2021-01-26 19:28:48 +0100

    edit dashboard view : removing header | edit dashboard css | edit _header view : adding header in it for all the page

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,3 +1,30 @@
 <header>
-    
+<nav class="navbar navbar-expand-md bg-dark">
+        <ul class="navbar nav">
+            <li class="nav-item">
+                <a class="nav-link text-light font-weight-bold text-uppercase px-3" href="#">Menu</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link text-light font-weight-bold text-uppercase px-3" href="#">Stock</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-weight-bold text-uppercase px-3" href="#">Factures</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-weight-bold text-uppercase px-3" href="#">Stats</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-weight-bold text-uppercase px-3" href="#">Clients</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-weight-bold text-uppercase px-3" href="#">Marques</a>
+            </li>
+
+            <li class="nav-item"><a href="{{route('signout')}}">Deconnexion</a></li>
+        </ul>
+        
+            </div>
+        </form>
+    </nav>
+
 </header>

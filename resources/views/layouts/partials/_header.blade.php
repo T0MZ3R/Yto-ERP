26a49163d45d2a6c4b2163d069aabad45df2cb23
YVES <yves.tshimangakabidi@ynov.com>
2021-01-28 14:47:44 +0100

    adding route to the link of the dashboard

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -2,16 +2,16 @@
 <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">YTO</a>
+                <a class="navbar-brand" href="{{ route('dashboard') }}">YTO</a>
             </div>
             <ul class="nav navbar-nav">
-                <li><a href="#">Stock</a></li>
-                <li><a href="#">Facture</a></li>
-                <li><a href="#">Clients</a></li>
-                <li><a href="#">Marques</a></li>
+                <li><a href="{{ route('indexStock') }}">Stock</a></li>
+                <li><a href="{{ route('indexFacture') }}">Facture</a></li>
+                <li><a href="{{ route('indexClient') }}">Clients</a></li>
+                <li><a href="{{ route('indexMarque') }}">Marques</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user">Deconnexion</a></li>
+                <li><a href="{{ route('signout') }}"><span class="glyphicon glyphicon-user">Deconnexion</a></li>
 
             </ul>
         </div>

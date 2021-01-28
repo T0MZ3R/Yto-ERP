28518fb4316884f36fc6fb127dadf06eb10f61e3
T0MZ3R <tom.aitouarab@ynov.com>
2021-01-28 18:43:55 +0100

    fix header (adding logout logo)

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,5 +1,5 @@
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
           <a class="navbar-brand" href="{{ route('dashboard') }}">YTO</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,7 +8,7 @@
           <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active"  href="{{ route('indexStock') }}">Stocks</a>
+                <a class="nav-link"  href="{{ route('indexStock') }}">Stocks</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="{{ route('indexFacture') }}">Factures</a>
@@ -17,13 +17,17 @@
                 <a class="nav-link" href="{{ route('indexClient') }}">Clients</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active"  href="{{ route('indexMarque') }}">Marques</a>
+                <a class="nav-link" href="{{ route('indexMarque') }}">Marques</a>
               </li>
               
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ route('signout') }}"><span class="glyphicon glyphicon-user">Deconnexion</a></li>
-
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('signout') }}">
+                        <img class="logout" alt="OHHHHH" srcset="/images/logout.svg">
+                        Deconnexion
+                    </a>
+                </li>
             </ul>
           </div>
         </div>

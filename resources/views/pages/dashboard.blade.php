c50a291d97a398232ca5441b1a30863e6cfe67ce
T0MZ3R <tom.aitouarab@ynov.com>
2021-01-28 16:58:56 +0100

    stylising dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -16,20 +16,23 @@
     <div class="mainContainer" id="stat">
         <div class="topBorder"></div>
         <div class="bottomBorder"></div>
-        <h2>Statistique</h2>
+        <div class="widgetTitle">
+            <h2>Statistique</h2>
+        </div>
     </div>
 
     <div class="mainContainer" id="fac">
         <div class="topBorder"></div>
         <div class="bottomBorder"></div>
-        <h2>Derniere facture</h2>
+        <div class="widgetTitle">
+            <h2>Derniere facture</h2>
+        </div>
         <table class="table table-hover table-bordered results">
-        <table>
             <tbody>
                 @for($i = 2; $i >= 0; $i--)
                 <tr>
-                    <td>{{ $lastFacture[$i]->created_at}} -</td>
-                    <td> {{ $lastFacture[$i]->price * $lastFacture[$i]->nb }}&euro;</td>
+                    <td>{{ $lastFacture[$i]->created_at}}</td>
+                    <td>{{ $lastFacture[$i]->price * $lastFacture[$i]->nb }}&euro;</td>
                     <td><a class="centered" href="{{ route('editClient', $lastFacture[$i]->id) }}"><img src="/images/print.png" width="20px" alt=""></a></td>
                 </tr>
                 @endfor
@@ -37,17 +40,23 @@
         </table>
     </div>
 
-    <div id="ca">
-        <h2>Chiffre de la journée</h2>
-        <p>{{ $ca }}&euro;</p>
+    <div id="ca">        
+        <div class="widgetTitle">
+            <h2>Chiffre de la journée</h2>
+        </div>
+        <p class="price">{{ $ca }}&euro;</p>
     </div>
     <div id="caw">
-        <h2>Chiffre semaine</h2>
-        <p>{{ $caw }}&euro;</p>
+        <div class="widgetTitle">
+            <h2>Chiffre semaine</h2>
+        </div>
+        <p class="price">{{ $caw }}&euro;</p>
     </div>
     <div id="cam">
-        <h2>Chiffre du mois</h2>
-        <p>{{ $cam }}&euro;</p>
+        <div class="widgetTitle">
+            <h2>Chiffre du mois</h2>
+        </div>
+        <p class="price">{{ $cam }}&euro;</p>
     </div>
 </div>
 @endsection

cdd3e8b542cfef82fa52aff6380a6dd9861e7db7
T0MZ3R <tom.aitouarab@ynov.com>
2021-01-28 03:22:36 +0100

    Adding to dashboard  3 last facture | Ca of the day | Ca of the week | Ca of the month

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Stock;
+use App\Models\Marque;
+use App\Models\Client;
+use App\Models\Facture;
 
 class PageController extends Controller
 {
     //
     public function dashboard(){
-        return view('pages/dashboard');
+        // $facture = Facture::all();
+        // $stock = Stock::all();
+        // $client = Client::all();
+        // $marque = Marque::all();
+
+        // Get 3 last facture
+        $lastFacture = Facture::latest()->take(3)->get();
+
+        // Get ca of the day
+        $todaysFacture = Facture::all()->where('created_at', '>=', \Carbon\Carbon::today());
+        $ca = 0;
+        foreach($todaysFacture as $item){
+            $ca += $item->nb * $item->price;
+        }
+
+        // Get ca of the week
+        function getStartAndEndDate($week, $year) {
+            $dto = new \DateTime();
+            return $dto->setISODate($year, $week)->format('Y-m-d');
+        }
+
+        $thisWeekFacture = Facture::all()->where('created_at', '>=', getStartAndEndDate(date('W'),date('Y')).' 00:00:00');
+        $caw = 0;
+        foreach($thisWeekFacture as $item){
+            $caw += $item->nb * $item->price;
+        }
+
+        // Get ca of the month
+        $thisMonthFacture = Facture::all()->where('created_at', '>=', date('Y').'-'.date('m').'-'.'01 00:00:00');
+        $cam = 0;
+        foreach($thisMonthFacture as $item){
+            $cam += $item->nb * $item->price;
+        }
+        
+        return view('pages/dashboard',compact('lastFacture','ca','caw','cam'));
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,25 +3,41 @@
 @section('assets')
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/dashboard.css">
-
-    <script src="https://kit.fontawesome.com/if7457abdb.js"></script>
+<link rel="stylesheet" href="assets/css/dashboard.css">
     
 @endsection
 
 @section('content')
-
-    
-    
-
-    <div id="conteneur">
-        <div id="stat" >Statistique <a href="#"></a></div>
-        <div id="fac">Derniere facture</div>
-        <div id="fav">Favoris</div>
-        <div id="ca">chiffre de la journee</div>
-        <div id="cam">chiffre du mois </div>
-        <div id="log">Log</div>
+<div id="container">
+    <div id="stat">
+        <h2>Statistique</h2>
     </div>
-    
+    <div id="fac">
+        <h2>Derniere facture</h2>
+        <table class="table table-hover table-bordered results">
+        <table>
+            <tbody>
+                @for($i = 2; $i >= 0; $i--)
+                <tr>
+                    <td>{{ $lastFacture[$i]->created_at }}</td>
+                    <td>{{ $lastFacture[$i]->price * $lastFacture[$i]->nb }}&euro;</td>
+                    <td><a class="centered" href="{{ route('editClient', $lastFacture[$i]->id) }}"><img src="/images/print.png" width="20px" alt=""></a></td>
+                </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
+    <div id="ca">
+        <h2>Chiffre de la journée</h2>
+        <p>{{ $ca }}&euro;</p>
+    </div>
+    <div id="caw">
+        <h2>Chiffre semaine</h2>
+        <p>{{ $caw }}&euro;</p>
+    </div>
+    <div id="cam">
+        <h2>Chiffre du mois</h2>
+        <p>{{ $cam }}&euro;</p>
+    </div>
+</div>
 @endsection

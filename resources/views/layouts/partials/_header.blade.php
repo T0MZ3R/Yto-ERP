e05df3aea95070c1471344ed3eafa2c72b96387e
YVES <yves.tshimangakabidi@ynov.com>
2021-01-28 17:52:05 +0100

    bugfix on header style

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,20 +1,32 @@
 <header>
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="{{ route('dashboard') }}">YTO</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="{{ route('indexStock') }}">Stock</a></li>
-                <li><a href="{{ route('indexFacture') }}">Facture</a></li>
-                <li><a href="{{ route('indexClient') }}">Clients</a></li>
-                <li><a href="{{ route('indexMarque') }}">Marques</a></li>
+          <a class="navbar-brand" href="{{ route('dashboard') }}">YTO</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active"  href="{{ route('indexStock') }}">Stocks</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('indexFacture') }}">Factures</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('indexClient') }}">Clients</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active"  href="{{ route('indexMarque') }}">Marques</a>
+              </li>
+              
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="{{ route('signout') }}"><span class="glyphicon glyphicon-user">Deconnexion</a></li>
 
             </ul>
+          </div>
         </div>
-    </nav>
+      </nav>
 
 </header>

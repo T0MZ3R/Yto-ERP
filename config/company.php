bae04b67234ea6dd4ec0c2b15cc391b772e34de6
T0MZ3R <tom.aitouarab@ynov.com>
2021-01-28 19:26:36 +0100

    creating config file company

---
+++ config/company.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => env('COMPANY_name', 'T-Corp'),
+    'address' => env('COMPANY_Address', '8 avenue des Champs Elysée'),
+    'city' => env('COMPANY_City', '75000 Paris'),
+    'phone' => env('COMPANY_Phone', '01 00 00 00 00'),
+    'mail' => env('COMPANY_Mail', 'contact@website.com'),
+    'website' => env('COMPANY_Website', 'www.website.com'),
+];
